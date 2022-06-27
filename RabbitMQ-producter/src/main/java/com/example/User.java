4875e28291467c711d95edbc4dbdfcc381ecbea1
王志勇 <1203836469@qq.com>
2022-06-27 15:26:24 +0800

    MQ发送对象消息

---
+++ RabbitMQ-producter/src/main/java/com/example/User.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author wangzy
+ * @date 2022/6/27 15:16
+ *
+ * 实现了Serializable的才能被序列化
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    private String name;
+    private String age;
+    private Double altitude;
+}
--- RabbitMQ-producter/src/test/java/com/example/RabbitTest.java
+++ RabbitMQ-producter/src/test/java/com/example/RabbitTest.java
@@ -25,6 +25,19 @@ public class RabbitTest {
         String message = "这是一条消息";
 
         rabbitTemplate.convertAndSend(queueName,message);
+        //convertAndSend能自动将
     }
 
+    @Test
+    public void test2(){
+        User user = new User("小王王", "16", 180.5);
+        //队列名
+        String queueName = "hello_world";
+
+        //convertAndSend，会自动将传的消息Object转换为一个Message，
+        //并且这个对象的类实现了Serializable接口才能直接发对象作为消息，不然会报错
+        //SimpleMessageConverter only supports String, byte[] and Serializable payloads, received: com.example.User
+        //user类implements Serializable后能解决这个报错
+        rabbitTemplate.convertAndSend(queueName,user);
+    }
 }
